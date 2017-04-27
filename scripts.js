87f4b5139eb823c6de8d093d6bb8b695cf0b2f02
Bartłomiej Kowalówka <bartekowa@gmail.com>
2017-04-27 19:36:55 +0200

    plus or minus

---
+++ scripts.js
@@ -0,0 +1,13 @@
+var a = 2
+var b = 5
+var value = (a * a) + (2 * a * b) - (b * b)
+
+console.log('Wynik dla wzoru dla liczb: ' + a + ' i : ' + b + ' wynosi: ' + value);
+
+var whatIsBigger = value > 0 ? 'Wynik jest dodatni' : 'Wynik jest ujemny';
+console.log(whatIsBigger);
+
+var equalZero = value = 0 ? 'Wynik jest równy 0' : 'Wynik nie jest równy 0';
+console.log(equalZero)
+
+
